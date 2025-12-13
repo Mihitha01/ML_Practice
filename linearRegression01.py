af3d115b14f290bc9c33e8e8d814d4dd816da430
Mihitha01 <mihithabandara@gmail.com>
2025-12-13 23:52:30 +0530

    Implement linear regression model with prediction in linearRegression01.py

--- linearRegression01.py
+++ linearRegression01.py
@@ -1,1 +1,13 @@
-from sklearn import 
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+x = np.array([1, 2, 3, 4, 5]).reshape(-1, 1)
+y = np.array([5, 7, 9, 11, 13])
+
+model = LinearRegression()
+
+model.fit(x, y)
+
+prediction = model.predict([[10]])
+
+print(prediction)
