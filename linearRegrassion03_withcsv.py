1787c64e44b4d7331f166ec95ab9fc48876b3478
Mihitha01 <mihithabandara@gmail.com>
2025-12-14 23:27:23 +0530

    Updated the csv file

--- linearRegrassion03_withcsv.py
+++ linearRegrassion03_withcsv.py
@@ -3,6 +3,6 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('C:/Users/User/Downloads/landPrice.csv')
+df = pd.read_csv('C:/Users/User/Downloads/landPrice2.csv')
 
 print(df.head())
