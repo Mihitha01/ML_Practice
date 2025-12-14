ef1e2110d3737e26bc8cf2b91712096234f4d4a7
Mihitha01 <mihithabandara@gmail.com>
2025-12-14 23:32:32 +0530

    Refactor linear regression script to improve data handling and output clarity in linearRegrassion03_withcsv.py

--- linearRegrassion03_withcsv.py
+++ linearRegrassion03_withcsv.py
@@ -3,6 +3,24 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('C:/Users/User/Downloads/landPrice2.csv')
+data = pd.read_csv('C:/Users/User/Downloads/landPrice2.csv')
 
-print(df.head())
+print(data.head())
+
+df = pd.DataFrame(data)
+
+X = df[['size_sqft']]
+y = df['price_k']
+
+model = LinearRegression()
+
+model.fit(X, y)
+
+prediction = model.predict(pd.DataFrame({'size_sqft': [1800]}))
+
+print("Predicted price for 1800 sqft land (in thousands):", prediction)
+
+print("Slope (price per sqft):", model.coef_)
+
+print("Intercept:", model.intercept_)
+
