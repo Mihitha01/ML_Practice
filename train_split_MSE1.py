11d1053f7310d2e27638c2f72c9d3ec23cc8f781
Mihitha01 <mihithabandara@gmail.com>
2025-12-15 12:28:29 +0530

    Refactor CSV loading to check current directory before alternate path in train_split_MSE1.py

--- train_split_MSE1.py
+++ train_split_MSE1.py
@@ -4,8 +4,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
+import os
 
-data = pd.read_csv('C:/Users/User/Downloads/landPrice2.csv')
+# Try to load CSV - check current directory first
+csv_path = 'landPrice2.csv'
+if not os.path.exists(csv_path):
+    # Try alternate path
+    csv_path = 'C:/Users/User/Downloads/landPrice2.csv'
+
+data = pd.read_csv(csv_path)
 
 df = pd.DataFrame(data)
 
@@ -15,5 +22,14 @@ y = df["price_k"]
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 model = LinearRegression()
-
 model.fit(x_train, y_train)
+
+prediction = model.predict(pd.DataFrame({"size_sqft": [1800]}))
+
+print(prediction)
+
+y_pred = model.predict(x_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error on test set:", mse)
+
