2d2f64e2f79f515a9a687474a44b5abde890e901
Mihitha01 <mihithabandara@gmail.com>
2025-12-16 19:44:28 +0530

    Add random forest regression script for house price prediction

---
+++ randomForest01.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+df = pd.read_csv("house_prices_multi.csv")
+
+x = df[["size_sqft", "bedrooms", "age_years"]]
+y = df["price_k"]  
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+model.fit(x_train, y_train)
+
+y_perd = model.predict(x_test)
+
+mse = mean_squared_error(y_test, y_perd)
+
+print("Mean Squared Error: ", mse)
+
+new_house = [[2500, 4, 5]]  # Example: 2500 sqft, 4 bedrooms, 5 years old
+
+prediction = model.predict(new_house)
+
+print("Predicted price (in $1000s): ", prediction)
+
